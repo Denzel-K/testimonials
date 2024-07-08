8f2d4da51cd0e33445ed9a6e4b42f0db5db9894d
Denzel <denzelk741@gmail.com>
2024-07-08 15:25:28 +0300

    updated

--- script.js
+++ script.js
@@ -1,45 +1,21 @@
-const previous = document.querySelectorAll('.previous');
-const next = document.querySelectorAll('.nextone');
+const toTakpor = document.querySelectorAll('.toTakpor');
+const toTanya = document.querySelectorAll('.toTanya');
 const Tanya = document.querySelector('.Tanya');
 const Tarkpor = document.querySelector('.Tarkpor');
-const previousbtn = document.querySelectorAll('.prev');
-const nextbtn = document.querySelectorAll('.next');
-const badilisha = document.querySelectorAll('.slide_change');
+// const badilisha = document.querySelectorAll('.slide_change');
 
 
-next.forEach(nex => {
+toTakpor.forEach(nex => {
     nex.addEventListener ('click', ()=> {
         Tarkpor.classList.remove ('hidden');
         Tanya.classList.add('hidden');
-
-        badilisha.forEach(state => {
-            state.classList.add ('reverse-btns');
-        })
-
-        nextbtn.forEach (ahead => {
-            ahead.classList.add ('rotating');
-        })
-        previousbtn.forEach (behind => {
-            behind.classList.add ('rotating');
-        })
     })
 })
 
-previous.forEach(prev => {
+toTanya.forEach(prev => {
     prev.addEventListener ('click', ()=> {
         Tarkpor.classList.add ('hidden');
         Tanya.classList.remove('hidden');
-
-        badilisha.forEach(state => {
-            state.classList.remove ('reverse-btns');
-        })
-
-        nextbtn.forEach (ahead => {
-            ahead.classList.remove ('rotating');
-        })
-        previousbtn.forEach (behind => {
-            behind.classList.remove ('rotating');
-        })
     })
 })
 
